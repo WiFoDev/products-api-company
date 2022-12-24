da7409759e062255e15935ff4b52553effc80061
WiFoDev <lou81804@gmail.com>
2022-12-23 19:23:03 -0500

    add isAdmin token middleware

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -6,6 +6,8 @@ import { db } from '../db';
 import { Payload } from './types/Payload';
 import { AuthRequest } from './types/AuthRequest';
 import { UserNotFoundException } from '../Exceptions/UserNotFoundException';
+import { Role } from '@prisma/client';
+import { NotAuthorizedException } from '../Exceptions/NotAuthorizedException';
 
 export const verifyToken = async (
   req: AuthRequest,
@@ -33,6 +35,27 @@ export const verifyToken = async (
       error instanceof UserNotFoundException
     ) {
       res.status(401).json({ status: 'fail', error: error.message });
+    } else {
+      res.status(500).json({ status: 'fail', error });
+    }
+  }
+};
+
+export const isAdmin = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: req.id },
+      select: { role: true }
+    });
+    if (user?.role !== Role.ADMIN) throw new NotAuthorizedException();
+    next();
+  } catch (error) {
+    if (error instanceof NotAuthorizedException) {
+      res.status(403).json({ status: 'fail', error: error.message });
     } else {
       res.status(500).json({ status: 'fail', error });
     }
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -6,13 +6,13 @@ import {
   getProducts,
   updateProduct
 } from '../controllers/products.controller';
-import { verifyToken } from '../middlewares/verifyToken';
+import { isAdmin, verifyToken } from '../middlewares/verifyToken';
 
 const router = Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', verifyToken, createProduct);
+router.post('/', [verifyToken, isAdmin], createProduct);
 router.patch('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
